8172f2363b4a301b0fc161b56c214ccfc377c0db
dhthanh <thanh.dh@ou.edu.vn>
2016-12-07 16:09:31 +0700

    Init project

---
+++ frmCustomer.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Bai9_DisConnect
+{
+    partial class frmCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgvCustomer = new System.Windows.Forms.DataGridView();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dgvCustomer);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnCancel);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSave);
+            this.splitContainer1.Size = new System.Drawing.Size(613, 516);
+            this.splitContainer1.SplitterDistance = 448;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dgvCustomer
+            // 
+            this.dgvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCustomer.Location = new System.Drawing.Point(0, 0);
+            this.dgvCustomer.Name = "dgvCustomer";
+            this.dgvCustomer.Size = new System.Drawing.Size(613, 448);
+            this.dgvCustomer.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(307, 16);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 36);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(207, 16);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 36);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 516);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmCustomer";
+            this.Text = "frmCustomer";
+            this.Load += new System.EventHandler(this.frmCustomer_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgvCustomer;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ frmCustomer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Bai9_DisConnect
+{
+    public partial class frmCustomer : Form
+    {
+        string cnStr = "";
+        SqlConnection cn;
+        DataSet ds;
+
+        public frmCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomer_Load(object sender, EventArgs e)
+        {
+            cnStr = ConfigurationManager.ConnectionStrings["cnStr"].ConnectionString;
+            cn = new SqlConnection(cnStr);
+            dgvCustomer.DataSource = GetCustomerDataset().Tables[0];
+        }
+        
+        public DataSet GetCustomerDataset()
+        {
+            try
+            {
+                string sql = "SELECT * FROM KhachHang";
+                SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(); //=>UPDATE
+            
+            //////////////INSERT
+            //string ins = "INSERT INTO KhachHang(MaKH, TenKH, DiaChi, DienThoai, Fax) VALUES(@id, @name, @address, @phone, @fax)";
+            string ins = "INSERT INTO KhachHang(MaKH) VALUES(@id)";                        
+            SqlCommand cmd = new SqlCommand(ins, cn);
+            cmd.Parameters.Add("@id", SqlDbType.NVarChar, 4, "MaKH");
+            //cmd.Parameters.Add("@id", SqlDbType.NVarChar, 4, "MaKH");
+            da.InsertCommand = cmd;           
+            
+            /////////////DELETE
+            string del = "DELETE FROM KhachHang WHERE MaKH = @id";
+            cmd = new SqlCommand(del, cn);
+            cmd.Parameters.Add("@id", SqlDbType.NVarChar, 4, "MaKH");
+            da.DeleteCommand = cmd;
+            
+            /////////////UPDATE
+            //
+            ////////////
+            da.Update(ds);            
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {            
+            ds.RejectChanges();
+        }
+    }
+}
